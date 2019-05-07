85f9ab91e1baa86596ae7b5b6b265b420f52d4af
Jason <haloos@me.com>
2019-05-07 13:55:36 -0400

    added props

---
+++ src/robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Jason P',
+    username: 'Shit lips',
+    email: 'Sincere@april.biz'
+  },
+  {
+    id: 2,
+    name: 'Laura G', 
+    username: 'DJ Xtacy', 
+    email: 'dontknow@boo'
+  },
+  {
+    id: 3,
+    name: 'DJ Xtacy',
+    username: 'weirdo',
+    email:'imweird@yo'
+
+  }, 
+  {
+    id: 4,
+    name: 'Claudia B',
+    username: 'dj t',
+    email:'mrmagoo@yo'
+  }, 
+  {
+    id: 5,
+    name: 'Nitin K',
+    username: 'truck',
+    email:'po@me'
+  }, 
+  {
+    id: 6,
+    name: 'Jessica R',
+    username: 'cartoon ',
+    email:'imweird@me'
+  }, 
+  {
+    id: 7,
+    name: 'Nigel W',
+    username: 'oic',
+    email:'2cool@biz'
+  }, 
+  {
+    id: 8,
+    name: 'Alice N',
+    username: 'baby cakes',
+    email:'imsuper@yo'
+  }, 
+  {
+    id: 9,
+    name: 'Cassandra M',
+    username: 'teacher',
+    email:'coder@it'
+  }, 
+  {
+    id: 10,
+    name: 'Jane G',
+    username: 'truck',
+    email:'imcool@no'
+  },
+];
