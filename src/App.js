936e22961c64b2e8ff59dbc80b53cabc5a655645
Jason <haloos@me.com>
2019-05-11 12:05:00 -0400

    added this.state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React from 'react'; 
+import React, { Component } from 'react'; 
 import Cardlist from './Cardlist'; 
 import { robots } from './robots'; 
-import Searchbox from './Searchbox';
+import Searchbox from './Searchbox';  
 
-const App = () => {
-  return ( 
-    <div className='tc'> 
-      <h1>RoboFriends</h1> 
-      <Searchbox/>
-      <Cardlist robots={robots}/> 
-    </div>
-  );
+
+class App extends Component { 
+  constructor() { 
+    super()
+    this.state = {
+      robots: robots,
+      searchfield: '',
+    }
+  } 
+
+
+  render() {
+    return ( 
+      <div className='tc'> 
+        <h1>RoboFriends</h1> 
+        <Searchbox/>
+        <Cardlist robots={this.state.robots}/> 
+      </div>
+    );
+  }
+}
   
-}  
-
 export default App; 
