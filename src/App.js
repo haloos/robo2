df52f83187ec8f5a22e37502421f6163835fbe0c
Jason <haloos@me.com>
2019-05-16 11:19:32 -0400

    added componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'; 
 import Cardlist from './Cardlist'; 
-import { robots } from './robots'; 
 import Searchbox from './Searchbox';  
 import './App.css'; 
 
@@ -9,10 +8,22 @@ class App extends Component {
   constructor() { 
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     }
-  } 
+  }   
+
+  componentDidMount() { 
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response=> {
+        return response.json(); 
+    })  
+
+    .then(users => {
+      this.setState({ robots: users });
+   });
+  
+  }
 
   onSearchChange = (event) => { 
       this.setState({ searchfield: event.target.value })
